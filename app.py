ce7258a825821b2513e3c6354554323ab3493734
NikitaOrehov <Ornik0005@yandex.ru>
2025-04-11 14:53:02 +0300

    add chats

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import array
 from flask import Flask, jsonify, request
 import psycopg2
 from urllib.parse import urlparse
@@ -67,7 +68,7 @@ def info(login):
     if request.method == 'GET':
         try:
             curs = conn.cursor()
-            curs.execute("SELECT name, surname, databirth, phone, location, exp_alc, record, id FROM users WHERE login = %s", (login,))
+            curs.execute("SELECT u.name, u.surname, u.databirth, u.phone, u.location, u.exp_alc, u.record, u.id, u.fav_alc, c.linex, c.liney, c.histogramma, c.circules FROM users u RIGHT JOIN charts c ON u.id = c.id WHERE u.login = %s;", (login,))
             res = curs.fetchone()
             answer = {
                 'name': res[0].strip(),
@@ -76,18 +77,35 @@ def info(login):
                 'phone': res[3],
                 'location': res[4],
                 'exp_alc': res[5], 
-                'record': res[6]
+                'record': res[6],
+                'fav_alc' : res[8],
+                'login' : login,
+                'linex' : res[9],
+                'liney' : res[10],
+                'histogramma' : res[11],
+                'circules' : res[12]
                 }
             _id = res[7]
             curs.execute("SELECT pred FROM PREDILECTION WHERE id = %s", (_id,))
             res = curs.fetchall()
             predilection = [i[0].strip() for i in res]
-            print(predilection)
             answer['pred'] = predilection
             curs.execute("SELECT event FROM achievements WHERE id = %s", (_id,))
             res = curs.fetchall()
             achievements = [i[0].strip() for i in res]
             answer['achiev'] = achievements
+            curs.execute('SELECT * FROM chats WHERE id_person_1 = %s OR id_person_2 = %s', (_id, _id))
+            res = curs.fetchall()
+            chats = {}
+            for i in res.count():
+                chat = []
+                curs.execute('SELECT id_message, id_send, text, data FROM message WHERE id_chat = %s', (chats[i]))
+                messages = curs.fetchall()
+                for j in messages:
+                    chat.append(j)
+                chats[chats[i]] = chat
+            answer[chats]
+
             return answer
         except psycopg2.Error as e:
             print(f"Database query error: {e}")
@@ -97,21 +115,32 @@ def info(login):
         try:
             curs = conn.cursor()
             date = data.get('databirth')
+            exp_alc = data.get('exp_alc')
+            record = data.get('record')
             if date == "":
-                date = NULL
-            curs.execute("UPDATE users SET name = %s, surname = %s, databirth = %s, login = %s, phone = %s, location = %s, exp_alc = %s, record = %s WHERE login = %s", (data.get('name'), data.get('surname'), date, data.get('login'), data.get('phone'), data.get('location'), data.get('exp_alc'), data.get('record'), login))
-            curs.execute("UPDATE users SET name = 'Nik' WHERE id = 1")
-            res = curs.fetchall()
-            print(res)
-            print(res[0])
-            return jsonify({'er' : res[0]})
+                date = None
+            if record == "":
+                record = None
+            if exp_alc == "":
+                exp_alc = None
+            arrayPred = data.get('pred')
+            arrayAchiev = data.get('achiev')
+            curs.execute("SELECT id FROM users WHERE login = %s", (login, ))
+            _id = curs.fetchone()[0]           
+            curs.execute("UPDATE users SET name = %s, surname = %s, login = %s, phone = %s, location = %s, exp_alc = %s, record = %s, fav_alc = %s WHERE login = %s", 
+                         (data.get('name'), data.get('surname'), data.get('login'), data.get('phone'), data.get('location'), data.get('exp_alc'), record, data.get('fav_alc'), login))                                     
+            curs.execute("DELETE FROM predilection WHERE id = %s", (_id, ))
+            curs.execute("INSERT INTO predilection (id, pred) VALUES (%s, %s), (%s, %s), (%s, %s), (%s, %s)", (_id, arrayPred[0], _id, arrayPred[1], _id, arrayPred[2], _id, arrayPred[3]))
+            curs.execute("DELETE FROM achievements WHERE id = %s", (_id, ))
+            curs.execute("INSERT INTO achievements (id, event) VALUES (%s, %s), (%s, %s), (%s, %s), (%s, %s)", (_id, arrayAchiev[0], _id, arrayAchiev[1], _id, arrayAchiev[2], _id, arrayAchiev[3]))
+            conn.commit()
+            return jsonify({'er' : 'ok'})
         except psycopg2.Error as e:
             print(f"Database query error: {e}")
             return jsonify({'error': 'Database error'}), 500
 
 
 
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
